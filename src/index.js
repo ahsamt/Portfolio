d50c17243d62184d44edf6036bcdc479942b5bbe
ahsamt <81040162+ahsamt@users.noreply.github.com>
2021-07-05 13:31:35 +0100

    added media queries

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hello");
+let aboutSection = document.querySelector("#about");
+let projectSection = document.querySelector("#projects");
+let contactSection = document.querySelector("#contact");
+
+function showProjects(event) {
+  event.preventDefault();
+  aboutSection.style.display = "none";
+  alert("hi");
+}
+
+document
+  .querySelector("#projects_button")
+  .addEventListener("click", showProjects);
