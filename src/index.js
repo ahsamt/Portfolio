915efd36a95576132e0cd1dacef14d7a7230856f
Maryia <tarpachova@gmail.com>
2022-04-12 13:09:39 +0100

    js added for the not deployed button

---
+++ src/index.js
@@ -0,0 +1,6 @@
+document.getElementById("no-data").addEventListener(("click"), (event) => say_sorry_not_ready(event));
+
+function say_sorry_not_ready(event) {
+    event.preventDefault();
+    alert("Sorry, this application has not yet been deployed. Please check again mid-May!")
+}
