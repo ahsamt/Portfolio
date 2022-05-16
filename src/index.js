67c5271c4da9c40d617972c4d2624f7c256d213f
Maryia <tarpachova@gmail.com>
2022-05-16 15:07:45 +0100

    updated alert for stockscreener (date)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ document.getElementById("no-data").addEventListener(("click"), (event) => say_so
 
 function say_sorry_not_ready(event) {
     event.preventDefault();
-    alert("Sorry, this application has not yet been deployed. Please check again mid-May!")
+    alert("Sorry, this application has not yet been deployed. Please check again in June!")
 }
