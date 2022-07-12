82ffac5f6d20dc29dbfbc4efbf6ac168c6fc8250
Maryia <tarpachova@gmail.com>
2022-07-12 15:51:30 +0100

    js message updated for not deployed stockscreener

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ document.getElementById("no-data").addEventListener(("click"), (event) => say_so
 
 function say_sorry_not_ready(event) {
     event.preventDefault();
-    alert("Sorry, this application has not yet been deployed. Please check again in June!")
+    alert("Sorry, this application has not yet been deployed. Please check again in a couple of weeks!")
 }
