dafc3024eeb39faa8937692432279a4050ff2bdb
Maryia <tarpachova@gmail.com>
2022-07-16 23:25:45 +0100

    updated projects page

--- src/index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("no-data").addEventListener(("click"), (event) => say_sorry_not_ready(event));
-
-function say_sorry_not_ready(event) {
-    event.preventDefault();
-    alert("Sorry, this application has not yet been deployed. Please check again in a couple of weeks!")
-}
